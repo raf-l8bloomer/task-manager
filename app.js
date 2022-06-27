a1a2e3c3ee33886c8bc2858f7e1c3c5c40cdcebe
rafael.lumang <lumang.rafael@gmail.com>
2022-06-27 15:14:50 -0700

    adding Hide Completed filter

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const form = document.getElementById('form');
 const input = form.querySelector('input');
+
+const tasksDiv = document.querySelector('.tasks')
 const ul = document.getElementById('task-list');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label'); //must create label to identify this section
+const filterCheckbox = document.createElement('input');
+
+
+/* Creating the Hide Completed checkbox to toggle on or off and filter tasks*/
+filterLabel.textContent = "Hide Completed";
+filterCheckbox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild (filterCheckbox);
+tasksDiv.insertBefore(div, ul); //insert new div between .tasks and before ul
+filterCheckbox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked; //if filterCheckbox is checked, isChecked = true
+    const lis = ul.children; //traversing list items collection within ul
+    if (isChecked){
+        for (let i = 0 ; i = lis.length; i += 1) //for loop to go through each list item 
+        
+
+    }else {
+        for (let i = 0 ; i = lis.length; i += 1) //for loop to go through each list item 
+
+
+    }
+}
+
 
 
 /*Separating code into functions to make it more modular 
@@ -11,7 +39,9 @@ Write function to take the text inputted and return text as list item
 function createLI(text) {
     // creates list item from inputted text
     const li = document.createElement('li');
-    li.textContent = text;
+    const span = document.createElement('span'); //create span to convert text element to HTML element
+    span.textContent = text; 
+    li.appendChild(span);
     
     // adds checkbox to list item 
     const label = document.createElement('label');
@@ -88,8 +118,22 @@ ul.addEventListener ('click', (e) => {
         //remove li child from ul
         ul.removeChild(li);
        } else if (button.textContent === 'edit') { 
-        console.log('edit');
-        }
+            const span = li.firstElementChild; //locates li's first child element
+            const input = document.createElement('input'); //creates input element
+            input.type = 'text'; //assigning what kind of input 
+            input.value = span.textContent; //assigning the text inside span is the input value left now in editing state
+            li.insertBefore (input, span); //inserts the created input first before the span element ex: (new, existing)
+            li.removeChild(span); //removes existing span
+            button.textContent='save'; //changes 'edit' buton text to 'save'
 
+        } else if (button.textContent === 'save') { 
+            const input = li.firstElementChild; //located li's first child element
+            const span = document.createElement('span'); //creates span element
+            span.textContent = input.value; //the span's text content will now be what was inputted
+            li.insertBefore (span, input);// inserts the span before the input
+            li.removeChild(input); //removes input
+            button.textContent='edit'; //changes 'save' to 'edit'
+        }
      }
 })
+
